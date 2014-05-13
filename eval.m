4d17d9d7cd42158c009fa60db4427f47c25afb8b
lyjh <lyjh90735@gmail.com>
2014-05-12 23:33:36 -0400

    fix bugs in eval.m

--- eval.m
+++ eval.m
@@ -3,24 +3,25 @@ function evalDet(class)
 
 	[~, X1, Y1, X2, Y2, ~] = textread([class '_groundTruth.txt'], '%s %d %d %d %d %d');
 	[~, ~, x1, y1, x2, y2] = textread([class '_det.txt'], '%s %f %d %d %d %d');
-
+	[sc, si] = sort(-confidence);
 	N = size(X1, 1);
 
 	npos = 0; % number of positive samples
 	tp = zeros(N,1); % true positives
 	tp = zeros(N,1); % false positives
 
-	for i = 1:N
+	for j = 1:N
+		i = si(j);
 		gt = [X1(i), Y1(i), X2(i), Y2(i)];
 		det = [x1(i), y1(i), x2(i), y2(i)];
-		if gt == [0 0 0 0] && det == [0 0 0 0] % true negative
+		if all(gt == [0 0 0 0]) && all(det == [0 0 0 0]) % true negative
 			continue;
-		elseif gt == [0 0 0 0] && det ~= [0 0 0 0] % false positive
+		elseif all(gt == [0 0 0 0]) && all(det ~= [0 0 0 0]) % false positive
 			fp(i) = 1;
 			continue;
 		end
 		npos = npos+1;
-		if det ~= [0 0 0 0]
+		if all(det ~= [0 0 0 0])
 			a1 =  (gt(3)-gt(1)) * (gt(4)-gt(2));
 			a2 =  (det(3)-det(1)) * (det(4)-det(2));
 			xx1 = max(gt(1), det(:,1));
